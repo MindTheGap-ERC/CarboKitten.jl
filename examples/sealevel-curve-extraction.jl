335952871c800327af98b21d9f53b621d27b0aef
xyl96 <64159957+xyl96@users.noreply.github.com>
2023-10-20 16:54:33 +0200

    able to extract from private database like osf
    extract files from a private database and download it to local files. Still need an url, which makes mass pruduction of data less easy

--- examples/sealevel-curve-extraction.jl
+++ examples/sealevel-curve-extraction.jl
@@ -5,19 +5,30 @@ using Interpolations
 using DataFrames
 using CSV
 using HTTP
+#import OpenScienceFramework as OSF
 
 function sealevel(url)
-    #headers = Dict("Authorization" => "Bearer $token")
-    response = HTTP.get(url)# copy paste Sealelvel data from a database
+    #=osf = OSF.Client(; token = alexa)  # put your OSF token here
+    proj = OSF.project(osf; title="MIND THE GAP")
+    dir = OSF.directory(proj, "Analysis")
+    subdir = OSF.directory(dir, "CarboKitten")
+    subsubdir = OSF.directory(subdir, "OSF Storage")
+    file = OSF.file(subsubdir, name)
+    data = read(file)
+    url = OSF.url(file)
+    =#
+    headers = Dict("Authorization" => "Bearer $token")
+    response = HTTP.get(url,headers)# copy paste Sealelvel data from a database
     data = split(String(response.body), "\n")
-    data_float64 = parse.(Float64, data)
-    CSV.write("data/all-sealevel/sl.csv", DataFrame(sl=data_float64))
-
+    #data_float64 = parse.(Float64, data)
+    CSV.write("data/all-sealevel/sl.csv", DataFrame(sl=data))
 end
 
-url = "https://raw.githubusercontent.com/xyl96/sealevel_curve/main/stoch1" # extracting from an open url
+#url = "https://raw.githubusercontent.com/xyl96/sealevel_curve/main/stoch1" # extracting from an open url
+url = "https://osf.io/download/b956h/"
 #local_path = "data/all-sealevel/sl.csv"
-#token = "ohXRgQVgTj0bSR8hA9mItYdWVnK2m4gyRDjFFrfJDkHFlmt7PPVNFNFj0xtQUThLMSQUIJ"
+#name = "Auto000_Allo000_Stoch100V2.txt"
+token = "ohXRgQVgTj0bSR8hA9mItYdWVnK2m4gyRDjFFrfJDkHFlmt7PPVNFNFj0xtQUThLMSQUIJ";
 #localfile = "data/"
 sealevel(url) 
 #=t = 1 : 2500

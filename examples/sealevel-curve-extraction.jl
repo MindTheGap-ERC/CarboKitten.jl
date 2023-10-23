8304e5995a86002302656f2d6b36ff379b354727
xyl96 <64159957+xyl96@users.noreply.github.com>
2023-10-23 09:39:56 +0200

    Update sealevel-curve-extraction.jl

--- examples/sealevel-curve-extraction.jl
+++ examples/sealevel-curve-extraction.jl
@@ -1,5 +1,4 @@
 # sealevel curve extraction
-#module Import_SL
 
 using Interpolations
 using DataFrames
@@ -7,9 +6,9 @@ using CSV
 using HTTP
 #import OpenScienceFramework as OSF
 
-function sealevel(url)
+function sealevel(url,name)
     #=osf = OSF.Client(; token = alexa)  # put your OSF token here
-    proj = OSF.project(osf; title="MIND THE GAP")
+    proj = OSF.project(osf; title="CarbonKitten")
     dir = OSF.directory(proj, "Analysis")
     subdir = OSF.directory(dir, "CarboKitten")
     subsubdir = OSF.directory(subdir, "OSF Storage")
@@ -21,16 +20,15 @@ function sealevel(url)
     response = HTTP.get(url,headers)# copy paste Sealelvel data from a database
     data = split(String(response.body), "\n")
     #data_float64 = parse.(Float64, data)
-    CSV.write("data/all-sealevel/sl.csv", DataFrame(sl=data))
+    CSV.write("data/all-sealevel/$name.csv", DataFrame(sl=data))
 end
 
-#url = "https://raw.githubusercontent.com/xyl96/sealevel_curve/main/stoch1" # extracting from an open url
-url = "https://osf.io/download/b956h/"
+url = "https://osf.io/download/zntw8/"
 #local_path = "data/all-sealevel/sl.csv"
-#name = "Auto000_Allo000_Stoch100V2.txt"
-token = "ohXRgQVgTj0bSR8hA9mItYdWVnK2m4gyRDjFFrfJDkHFlmt7PPVNFNFj0xtQUThLMSQUIJ";
+name = "Auto000_Allo000_Stoch100V2"
+token = "the token";
 #localfile = "data/"
-sealevel(url) 
+sealevel(url,name) 
 #=t = 1 : 2500
 sl = sin.(2π .* t ./ 0.2)
 #file = open("data/all-sealevel/sl1.csv","w")

fad40c5b1fb0b8a3a4c53d513b0a5d4adbab7817
Liu <x.liu6@uu.nl>
2023-10-23 19:35:04 +0200

    use interpolation now

--- examples/plot-cap-extSL.jl
+++ examples/plot-cap-extSL.jl
@@ -1,13 +1,18 @@
 module Script
 using CarboKitten.Visualization
 using GLMakie
+import OpenScienceFramework as OSF
+
 
 function main()
     f = Figure()
     plot_crosssection(f[1, 1], "data/ca-extSL.h5")
-    save("docs/src/fig/b13-crosssection-ext.png", f)
+    save("C:/Users/Liu00141/OneDrive - Universiteit Utrecht/Desktop/work_by_week/external plots/b13-crosssection-ext.png", f)
+
 end
 end
 
 Script.main()
 # ~/~ end
+
+ 

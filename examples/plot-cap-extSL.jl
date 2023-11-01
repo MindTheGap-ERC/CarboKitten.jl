d937af40cc932aa151ffece4dd8aace52c53902e
xyl96 <64159957+xyl96@users.noreply.github.com>
2023-11-01 11:14:44 +0100

    Update plot-cap-extSL.jl

--- examples/plot-cap-extSL.jl
+++ examples/plot-cap-extSL.jl
@@ -7,7 +7,7 @@ import OpenScienceFramework as OSF
 function main()
     f = Figure()
     plot_crosssection(f[1, 1], "data/ca-extSL.h5")
-    save("C:/Users/Liu00141/OneDrive - Universiteit Utrecht/Desktop/work_by_week/external plots/b13-crosssection-ext.png", f)
+    save("you-path", f)
 
 end
 end

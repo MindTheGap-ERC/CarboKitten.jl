8e5d6cbe610ef7aebaabc871304fc44bec86cff1
xyl96 <64159957+xyl96@users.noreply.github.com>
2023-11-01 10:59:24 +0100

    minor changes to delete unused codes and stuff

--- examples/cap-extSL.jl
+++ examples/cap-extSL.jl
@@ -4,16 +4,16 @@ using DataFrames
 using CSV
 using Interpolations
 
+
 function sealevel_curve(t,filepath)
     data = DataFrame(CSV.File(filepath))
-    data = hcat(collect(0:length(data.sl).-1)./1000, data.sl)
+    data = hcat(collect(0:length(data.sl).-1)./1000, data.sl) #The output sealevel curve from R does not have time tab and have to add it 
     x = linear_interpolation(data[:,1], data[:,2])
     return x(t)
-end
+end 
 
-#CSV.read("data/all-sealevel/$name.csv", DataFrame, header=1)[!,1][round(Int,t*1000)+1]
 DEFAULT_INPUT = CaProd.Input(
-    sea_level = t -> sealevel_curve(t,"data/all-sealevel/sl1.csv"),
+    sea_level = t -> sealevel_curve(t,file_path),
     subsidence_rate=50.0,
     initial_depth=x -> x / 2.0,
     grid_size=(50, 100),
@@ -29,6 +29,11 @@ DEFAULT_INPUT = CaProd.Input(
     insolation=2000.0
 )
 
-CaProd.main(DEFAULT_INPUT, "data/ca-extSL.h5")
-# ~/~ end
-name = "Auto000_Allo000_Stoch100V2"
+folder_path = "data/all-sealevel"
+files = filter(x -> occursin(".csv", x), readdir(folder_path))
+
+for i in files
+    file_path = joinpath(folder_path,i)
+    name, _ = splitext(i)
+    CaProd.main(DEFAULT_INPUT, "data/ca-extSL-$name.h5")
+end
--- examples/sealevel-curve-extraction.jl
+++ examples/sealevel-curve-extraction.jl
@@ -10,10 +10,7 @@ url = "https://osf.io/5yka4/" #url of the folder
 dh.get(url,"data/all-sealevel")
 
 function sealevel()  
-    #headers = Dict("Authorization" => "Bearer $token")
-    #response = HTTP.get(url)# copy paste Sealelvel data from a database
-    #data = split(String(response.body), "\n")
-    #data_float64 = parse.(Float64, data)
+
     folder_path = "data/all-sealevel"
     files = readdir(folder_path)
     files = filter(x -> occursin(".txt", x), readdir(folder_path))
@@ -22,17 +19,10 @@ function sealevel()
         file_path = joinpath(folder_path,file)
         name, _ = splitext(file)
         data = split(read(file_path, String),"\n")
-        #readdlm(file_path,Float64)
         CSV.write("data/all-sealevel/$name.csv", DataFrame(sl = data))
     end
 
-    #CSV.write("data/all-sealevel/$name.csv", DataFrame(sl=data))
 end
 
-#local_path = "data/all-sealevel/sl.csv"
 sealevel() 
-#=t = 1 : 2500
-sl = sin.(2π .* t ./ 0.2)
-#file = open("data/all-sealevel/sl1.csv","w")
-CSV.write("data/all-sealevel/sl1.csv", DataFrame(sl=sl))
-close(file)=#
+

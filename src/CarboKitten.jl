80c6f1f75148289f3af1bfb4c6e7e8faab303b4d
Johan Hidding <j.hidding@esciencecenter.nl>
2024-01-15 16:10:41 +0100

    remove all non-essential dependencies

--- src/CarboKitten.jl
+++ src/CarboKitten.jl
@@ -3,8 +3,8 @@ module CarboKitten
 include("./Stencil.jl")
 include("./Burgess2013.jl")
 include("./Utility.jl")
-include("./BS92.jl")
+# include("./BS92.jl")
 include("./CaProd.jl")
-include("./Visualization.jl")
+# include("./Visualization.jl")
 
 end # module CarboKitten

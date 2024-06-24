5eb33251d9cf233a0d3362ad474dcf04618ecb10
Johan Hidding <j.hidding@esciencecenter.nl>
2024-06-24 11:58:56 +0200

    implement sediment buffer

---
+++ test/SedimentStackSpec.jl
@@ -0,0 +1,22 @@
+# ~/~ begin <<docs/src/sediment-buffer.md#test/SedimentStackSpec.jl>>[init]
+@testset "SedimentStack" begin
+  using CarboKitten.SedimentStack: push_sediment!, pop_sediment!
+  stack = zeros(Float64, 10, 3)
+  push_sediment!(stack, [5.0, 0, 0])
+  @test pop_sediment!(stack, 1.5) == [1.5, 0.0, 0.0]
+  push_sediment!(stack, [0.0, 2.0, 0.0])   # (0 0.5) (0 1) (0.5 0.5) (1 0) ...
+  @test pop_sediment!(stack, 2.0) == [0.25, 1.75, 0.0]
+  @test pop_sediment!(stack, 1.5) == [1.25, 0.25, 0.0]
+end
+
+@testset "SedimentArray" begin
+  using CarboKitten.SedimentStack: push_sediment!, peek_sediment
+  sediment = zeros(Float64, 10, 3, 5, 5)
+  for x in 1:10
+    production = rand(3, 5, 5)
+    push_sediment!(sediment, production)
+  end
+  a = peek_sediment(sediment, 1.0)
+  @test all(sum(a; dims=1) .≈ 1.0)
+end
+# ~/~ end

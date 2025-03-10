0a7a2908b6f551f1f1c0ca38dee123901658cfea
Johan Hidding <j.hidding@esciencecenter.nl>
2025-03-10 12:14:23 +0100

    fix examples that use transport model

--- examples/transport/active-layer.jl
+++ examples/transport/active-layer.jl
@@ -2,7 +2,6 @@
 module ActiveLayer
 
 using Unitful
-using CarboKitten.Stencil: convolution, stencil
 using CarboKitten.Config: Box, axes
 using CarboKitten.BoundaryTrait: Shelf
 using CarboKitten.Utility: in_units_of
@@ -18,7 +17,7 @@ using CarboKitten.Transport.ActiveLayer: pde_stencil, Amount, Rate
     production          # function (x::u"m", y::u"m") -> u"m/s"
     disintegration_rate::typeof(1.0u"m/Myr")
     subsidence_rate::typeof(1.0u"m/Myr")
-    diffusion_coefficient::typeof(1.0u"m")
+    diffusion_coefficient::typeof(1.0u"m/yr")
 end
 # ~/~ end
 # ~/~ begin <<docs/src/active-layer-transport.md#example-active-layer>>[1]
@@ -45,7 +44,7 @@ const input = Input(
     disintegration_rate = 50.0u"m/Myr",
     subsidence_rate = 50.0u"m/Myr",
 
-    diffusion_coefficient = 10000.0u"m"
+    diffusion_coefficient = 10.0u"m/yr"
 )
 # ~/~ end
 # ~/~ begin <<docs/src/active-layer-transport.md#example-active-layer>>[2]
@@ -68,21 +67,23 @@ function propagator(input)
     δ = Matrix{Amount}(undef, input.box.grid_size...)
     x, y = axes(input.box)
     μ0 = input.initial_topography.(x, y')
+    box = input.box
+    Δt = input.Δt
+    disintegration_rate = input.disintegration_rate
+    production = input.production
+    d = input.diffusion_coefficient
 
     function active_layer(state)
-        max_amount = input.disintegration_rate * input.Δt
+        max_amount = disintegration_rate * Δt
         amount = min.(max_amount, state.sediment)
         state.sediment .-= amount
 
-        input.production.(x, y') * input.Δt .+ amount
+        production.(x, y') * Δt .+ amount
     end
-
-    stc = pde_stencil(input.box, input.diffusion_coefficient)
-    apply_pde(μ::Matrix{Amount}, p::Matrix{Amount}) = stc(tuple.(μ, p), δ)
 
     function (state)
         p = active_layer(state)
-        apply_pde(state.sediment .+ μ0, p)
+        pde_stencil(box, Δt, d, δ, state.sediment .+ μ0, p)
         return Frame(state.time, δ)
     end
 end

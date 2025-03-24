314b026e835fbf3c8e7c00b3b0e7bbc79a528c00
Johan Hidding <j.hidding@esciencecenter.nl>
2025-03-24 16:16:02 +0100

    remove superfluous models; everything is covered in ALCAP

--- src/Components/ActiveLayer.jl
+++ src/Components/ActiveLayer.jl
@@ -2,72 +2,76 @@
 @compose module ActiveLayer
 @mixin WaterDepth, FaciesBase, SedimentBuffer
 
-export disintegration, transportation
+export disintegrator, transporter
 
 using ..Common
-using CarboKitten.Transport.ActiveLayer: pde_stencil
+using CarboKitten.Transport.Advection: transport
 using Unitful
 
 @kwdef struct Facies <: AbstractFacies
-    diffusion_coefficient::typeof(1.0u"m/yr")
+    diffusion_coefficient::typeof(1.0u"m/yr") = 0.0u"m/Myr"
+    wave_velocity = _ -> ((0.0u"m/Myr", 0.0u"m/Myr"), (0.0u"1/Myr", 0.0u"1/Myr"))
 end
 
 @kwdef struct Input <: AbstractInput
     disintegration_rate::Rate = 50.0u"m/Myr"
-end
-
-function define_h(input::AbstractInput,state::AbstractState)
-    max_h = input.disintegration_rate * input.time.Δt
-    w = water_depth(input)(state)
-    h = zeros(typeof(max_h), input.box.grid_size...)
-    for i in eachindex(input.box.grid_size)
-        if w[i] > 0.0u"m"
-            h[i] = min.(max_h, state.sediment_height[i])
-        end
-    end
-    return h
+    transport_solver = nothing
+    transport_substeps::Int = 1 
 end
 
 """
-    disintegration(input) -> f!
+    disintegrator(input) -> f!
 
 Prepares the disintegration step. Returns a function `f!(state::State)`. The returned function
 modifies the state, popping sediment from the `sediment_buffer` and returns an array of `Amount`.
 """
-function disintegration(input)
-    output = Array{Float64, 3}(undef, n_facies(input), input.box.grid_size...)
-    return function(state)
-        h = define_h(input, state)
+function disintegrator(input)
+    max_h = input.disintegration_rate * input.time.Δt
+    w = water_depth(input)
+    output = Array{Float64,3}(undef, n_facies(input), input.box.grid_size...)
+    depositional_resolution = input.depositional_resolution
+
+    return function (state)
+        wn = w(state)
+        h = min.(max_h, state.sediment_height)
+        h[wn.<=0.0u"m"] .= 0.0u"m"
         state.sediment_height .-= h
-        pop_sediment!(state.sediment_buffer, h ./ input.depositional_resolution .|> NoUnits, output)
-        return output .* input.depositional_resolution 
+        pop_sediment!(state.sediment_buffer, h ./ depositional_resolution .|> NoUnits, output)
+        return output .* depositional_resolution
     end
 end
 
 """
-    transportation(input::Input) -> f
+    transporter(input::Input) -> f
 
 Prepares the transportation step. Returns a function `f(state::State, active_layer)`,
 transporting the active layer, returning a transported `Amount` of sediment.
 """
-function transportation(input)
-    x, y = box_axes(input.box)
-    μ0 = input.initial_topography.(x, y')
-    # We always return this array
-    transported_output = Array{Amount,3}(undef, n_facies(input), input.box.grid_size...)
+function transporter(input)
+    solver = if input.transport_solver === nothing
+        runge_kutta_4(input.box)
+    else
+        input.transport_solver
+    end
+
+    w = water_depth(input)
     box = input.box
-    Δt = input.time.Δt
+    Δt = input.time.Δt / input.transport_substeps
+    steps = input.transport_substeps
     fs = input.facies
 
-    return function (state, active_layer::Array{Amount,3})
-        μ = state.sediment_height .+ μ0
-        for (i, f) in pairs(fs)
-            pde_stencil(box, Δt, f.diffusion_coefficient,
-                view(transported_output, i, :, :),
-                μ, view(active_layer, i, :, :))
-        end
+    return function (state, C::Array{Amount,3})
+        wd = w(state)
 
-        return transported_output
+        for (i, f) in pairs(fs)
+            for j in 1:steps
+                solver(
+                    (C, _) -> transport(
+                        input.box, f.diffusion_coefficient, f.wave_velocity,
+                        view(active_layer, i, :, :), wd),
+                    C, state.time, Δt)
+            end
+        end
     end
 end
 

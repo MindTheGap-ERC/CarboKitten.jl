94d67aace41b3f4f5dd1aed8d00d9111b44f4594
Johan Hidding <j.hidding@esciencecenter.nl>
2025-03-27 21:51:13 +0100

    allow specification of initial topography as a matrix

--- src/Components/WaterDepth.jl
+++ src/Components/WaterDepth.jl
@@ -20,9 +20,19 @@ function initial_state(input::AbstractInput)
     return State(step=0, sediment_height=zeros(Height, input.box.grid_size...))
 end
 
+function initial_topography(input::AbstractInput)
+    if input.initial_topography isa AbstractMatrix
+        @assert size(input.initial_topography) == input.box.grid_size
+        return input.initial_topography
+    end
+
+    x, y = box_axes(input.box)
+    return input.initial_topography.(x, y')
+end
+
 function water_depth(input::AbstractInput)
     x, y = box_axes(input.box)
-    eta0 = input.initial_topography.(x, y')
+    eta0 = initial_topography(input)
     sea_level = input.sea_level
     subsidence_rate = input.subsidence_rate
 
@@ -39,7 +49,7 @@ function write_header(fid, input::AbstractInput)
     x, y = box_axes(input.box)
     t = TimeIntegration.write_times(input)
 
-    gid["initial_topography"] = input.initial_topography.(x, y') |> in_units_of(u"m")
+    gid["initial_topography"] = initial_topography(input) |> in_units_of(u"m")
     gid["sea_level"] = input.sea_level.(t) .|> in_units_of(u"m")
     attr["subsidence_rate"] = input.subsidence_rate |> in_units_of(u"m/Myr")
 end

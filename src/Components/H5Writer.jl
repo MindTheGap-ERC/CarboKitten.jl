0e87108af027c03b592640fa72ac3a78b4a5bd30
Emilia Jarochowska <e.b.jarochowska@uu.nl>
2025-04-23 10:41:35 +0200

    change files because of entangled conflicts

--- src/Components/H5Writer.jl
+++ src/Components/H5Writer.jl
@@ -61,7 +61,7 @@
 
             create_dataset(fid, input)
             write_state(fid, 1, state)
-            
+
 		    run_model(Model{M}, input, state) do w, df 
                 write_frame(fid, w, df)
                 write_state(fid, w+1, state)

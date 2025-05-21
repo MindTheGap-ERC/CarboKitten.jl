71642ad11ff3d2ca42b8da00ab98b29c1736fe71
Liu <x.liu6@uu.nl>
2025-05-21 16:40:32 +0200

    visualise prod, depo and disint

--- examples/StraDiscrep_test/TestSCdiscrep.jl
+++ examples/StraDiscrep_test/TestSCdiscrep.jl
@@ -1,4 +1,3 @@
-
 using Test
 using CarboKitten
 using Unitful
@@ -11,6 +10,7 @@ using CarboKitten.Denudation
 using DataFrames
 import CSV as Official_CSV
 using HDF5: h5open
+using GLMakie
 const m = u"m"
 const Myr = u"Myr"
 
@@ -59,7 +59,7 @@ function main()
     H5Writer.run_model(Model{WDn}, INPUT, "$(PATH)/$(TAG).h5")
 
     data_export(
-        CSVCarbo(tuple.(10:20:30, 25),
+        CSVCarbo(tuple.(10:10:100, 25),
           :sediment_accumulation_curve => "$(PATH)/$(TAG)_sac.csv",
           :age_depth_model => "$(PATH)/$(TAG)_adm.csv",
           :stratigraphic_column => "$(PATH)/$(TAG)_sc.csv",
@@ -80,3 +80,65 @@ sc,sac = read_csvfiles(PATH,TAG)
 @test sum(sc.var"sc1_f1 [m]") ≈ sac.var"sac1 [m]"[end]
 
 
+## find the largest discrepancy in the y-axis
+function calculate_discrepancy(sc_col, sac_col)
+    sum(sc_col) ./sac_col[end]
+end
+
+function calculate_discrepancy_all(sc::DataFrame,sac::DataFrame)
+    discrepancy_matrix = Float64[]
+    for i in 2:size(sc)[2]
+        a = calculate_discrepancy(sc[:,i], sac[:,i])
+        push!(discrepancy_matrix, a)
+    end
+    return discrepancy_matrix
+end
+
+function find_discrepancy_location(SC::DataFrame,SAC::DataFrame)
+    discrepancy_matrix = calculate_discrepancy_all(sc, sac)
+    @show discrepancy_matrix
+    discrep_percent,location = findmax(discrepancy_matrix)
+    return (discrep_percent, location)
+end
+
+find_discrepancy_location(sc,sac)
+
+## extract deposition, production and disintegration with time from HDF5
+
+function extract_data_from_hdf5(FILE::String)
+    h5open(FILE, "r") do fid
+        production = read(fid["/production"], Float64)[1,100,25,:]
+        deposition = read(fid["/deposition"], Float64)[1,100,25,:]
+        disintegration = read(fid["/disintegration"], Float64)[1,100,25,:]
+    
+        @show size(production)
+    data_prod = DataFrame(
+        production = production,
+    )
+
+    data_depo = DataFrame(
+        deposition = deposition,
+    )   
+
+    data_dis = DataFrame(
+        disintegration = disintegration,
+    )
+
+    return data_prod, data_depo, data_dis
+    end
+
+end
+
+data_prod,data_depo,data_dis = extract_data_from_hdf5("$(PATH)/$(TAG).h5") 
+
+@test sum(data_depo.deposition) ≈ sum(data_prod.production) - sum(data_dis.disintegration)
+
+## Plotting production, deposition and disintegration
+fig = Figure()
+ax0 = Axis(fig[1, 1], xlabel="Time step", ylabel="Production [m]", title="Production")
+ax2 = Axis(fig[2, 1], xlabel="Time step", ylabel="Deposition [m]", title="Deposition")
+ax3 = Axis(fig[3, 1], xlabel="Time step", ylabel="Disintegration [m]", title="Disintegration")
+lines!(ax0,data_prod.production, label="Production", color=:blue)
+lines!(ax2,data_depo.deposition, label="Deposition", color=:red) 
+lines!(ax3,data_dis.disintegration, label="Disintegration", color=:green)
+save("$(PATH)/$(TAG).png",fig)

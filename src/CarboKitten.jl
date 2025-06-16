bb06179478bbabbc0826a099b3b8617e737675ef
Johan Hidding <j.hidding@esciencecenter.nl>
2025-06-16 14:39:07 +0200

    reexport Vec2 so that  tests pass

--- src/CarboKitten.jl
+++ src/CarboKitten.jl
@@ -104,9 +104,10 @@ include("./Testing.jl")
 using .Components.Common: in_units_of, @u_str
 using .Models: BS92, CAP, ALCAP
 using .BoundaryTrait: Boundary, Coast, Periodic, Reflected
+using GeometryBasics: Vec2
 
 export run_model, Box, box_axes, TimeProperties, time_axis,
        Model, BS92, CAP, ALCAP, in_units_of, @u_str,
-       AbstractBox, Boundary, Coast, Periodic, Reflected
+       AbstractBox, Boundary, Coast, Periodic, Reflected, Vec2
 
 end # module CarboKitten

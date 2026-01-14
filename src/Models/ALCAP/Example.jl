b1ee662044fbb75d7287330bd164098f7952f975
Johan Hidding <j.hidding@esciencecenter.nl>
2026-01-14 11:11:37 +0100

    entangled tangle

--- src/Models/ALCAP/Example.jl
+++ src/Models/ALCAP/Example.jl
@@ -17,21 +17,37 @@ const FACIES = [
         maximum_growth_rate=500u"m/Myr",
         extinction_coefficient=0.8u"m^-1",
         saturation_intensity=60u"W/m^2",
-        diffusion_coefficient=50.0u"m/yr"),
+        diffusion_coefficient=50.0u"m/yr",
+        name="euphotic"),
     ALCAP.Facies(
         viability_range = (4, 10),
         activation_range = (6, 10),
         maximum_growth_rate=400u"m/Myr",
         extinction_coefficient=0.1u"m^-1",
         saturation_intensity=60u"W/m^2",
-        diffusion_coefficient=25.0u"m/yr"),
+        diffusion_coefficient=25.0u"m/yr",
+        name="oligophotic"),
     ALCAP.Facies(
         viability_range = (4, 10),
         activation_range = (6, 10),
         maximum_growth_rate=100u"m/Myr",
         extinction_coefficient=0.005u"m^-1",
         saturation_intensity=60u"W/m^2",
-        diffusion_coefficient=12.5u"m/yr")
+        diffusion_coefficient=12.5u"m/yr",
+        name="aphotic"),
+
+    ALCAP.Facies(
+        active=false,
+        diffusion_coefficient=50.0u"m/yr",
+        name="euphotic transported"),
+    ALCAP.Facies(
+        active=false,
+        diffusion_coefficient=50.0u"m/yr",
+        name="oligophotic transported"),
+    ALCAP.Facies(
+        active=false,
+        diffusion_coefficient=50.0u"m/yr",
+        name="aphotic transported")
 ]
 
 const PERIOD = 0.2u"Myr"
@@ -51,6 +67,7 @@ const INPUT = ALCAP.Input(
     sea_level=t -> AMPLITUDE * sin(2π * t / PERIOD),
     subsidence_rate=50.0u"m/Myr",
     disintegration_rate=50.0u"m/Myr",
+    disintegration_transfer=p->[0.0u"m", 0.0u"m", 0.0u"m", p[1]+p[4], p[2]+p[5], p[3]+p[6]],
     insolation=400.0u"W/m^2",
     sediment_buffer_size=50,
     depositional_resolution=0.5u"m",

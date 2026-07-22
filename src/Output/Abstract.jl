36a2747c4a39efec012d03e0888bf232d226ab04
Lucy Salmon <lucy.salmon@adterra.com>
2026-07-22 11:44:43 +0200

    Fix getindex to include stratigraphic_column field added in #252

--- src/Output/Abstract.jl
+++ src/Output/Abstract.jl
@@ -6,7 +6,6 @@ import ...Algorithms: stratigraphic_column!
 
 export Data, DataColumn, DataSlice, DataVolume, Slice2, Header, DataHeader, Axes, AbstractOutput, Frame
 export parse_multi_slice, data_kind, new_output, add_data_set, set_attribute, state_writer, frame_writer, surface_heights
-export sediment_thickness, water_depth
 
 using Unitful
 using ...CarboKitten: OutputSpec, AbstractInput, AbstractState
@@ -47,15 +46,16 @@ end
     attributes::Dict{String,Any} = Dict()
 end
 
-@kwdef struct Data{F,D}
+@kwdef mutable struct Data{F,D}
     slice::Slice2
     write_interval::Int
     # Julia doesn't allow to say Array{Amount,D+1} here
     disintegration::Array{Amount,F}
     production::Array{Amount,F}
     deposition::Array{Amount,F}
-    bathymetry::Array{Amount,D}
+    sediment_thickness::Array{Amount,D}
     active_layer::Union{Array{Amount,F}, Nothing} = nothing
+    stratigraphic_column::Union{Array{Amount,F}, Nothing} = nothing
 end
 
 const DataVolume = Data{4,3}
@@ -84,8 +84,9 @@ Base.getindex(v::Data{F,D}, args...) where {F,D} =
             v.disintegration[:, args..., :],
             v.production[:, args..., :],
             v.deposition[:, args..., :],
-            v.bathymetry[args..., :],
-            v.active_layer == nothing ? nothing : v.active_layer[:, args..., :])
+            v.sediment_thickness[args..., :],
+            v.active_layer == nothing ? nothing : v.active_layer[:, args..., :],
+            nothing)  # stratigraphic_column: reset so it is recomputed for the slice
     end
 
 function parse_slice(s::AbstractString)
@@ -116,11 +117,14 @@ data_kind(spec::OutputSpec) = data_kind(spec.slice...)
 Given a data set, compute the stratigrahpic column.
 """
 function stratigraphic_column(data::Data{F, D}) where {F, D}
-    net_deposition = data.deposition .- data.disintegration
-    for c in eachslice(net_deposition, dims=(1:D...,))
-        stratigraphic_column!(c)
+    if data.stratigraphic_column === nothing
+        net_deposition = data.deposition .- data.disintegration
+        for c in eachslice(net_deposition, dims=(1:D...,))
+            stratigraphic_column!(c)
+        end
+        data.stratigraphic_column = net_deposition
     end
-    return net_deposition
+    return data.stratigraphic_column
 end
 
 """
@@ -130,23 +134,12 @@ Compute the water depth function for the given data set.
 """
 function water_depth(header::Header, data::Data{F, D}) where {F, D}
     na = [CartesianIndex()]
-    return header.sea_level[repeated(na, D-1)...,1:data.write_interval:end] .- data.bathymetry
-end
-
-"""
-    sediment_thickness(data)
-
-Compute the sediment thickness at each moment in the run by taking the cumulative
-sum of the net deposition (deposition - disintegration) at each moment.
-"""
-function sediment_thickness(data::Data{F, D}) where {F, D}
-    net_deposition = dropdims(sum(data.deposition .- data.disintegration, dims=1), dims=1)
-    for c in eachslice(net_deposition, dims=(1:D-1...,))
-        for i in 2:length(c)
-            c[i] += c[i-1]
-        end
-    end
-    return net_deposition
+    delta_t = header.axes.t[1:data.write_interval:end] .- header.axes.t[1]
+    sl = header.sea_level[1:data.write_interval:end]
+    h0 = header.initial_topography[data.slice..., na]
+    Δh = data.sediment_thickness
+    Σ = header.subsidence_rate .* delta_t[repeated(na, D-1)..., :]
+    return sl[repeated(na, D-1)...,:] .- h0 .- Δh .+ Σ
 end
 
 """
@@ -200,19 +193,18 @@ Set an attribute in the output object.
 function set_attribute end
 
 """
-    write_bathymetry(out::T, name::Symbol, idx::Int, data::AbstractArray{Amount, dim}) where {T, dim}
+    write_sediment_thickness(out::T, name::Symbol, idx::Int, data::AbstractArray{Amount, dim}) where {T, dim}
 
-Write the bathymetry to the output object. The `idx` should be corrected for
-write interval. That is, `idx` should range from `1` to `n_writes` for the named
-data set. This function should be implemented for 0, 1, and 2 dimensional
-arrays, corresponding to writing column, slice or volume data.
+Write the sediment thickness to the output object. The `idx` should be corrected for write
+interval. That is, `idx` should range from `1` to `n_writes` for the named data set. This
+function should be implemented for 0, 1, and 2 dimensional arrays, corresponding to writing
+column, slice or volume data.
 
-If your output object type doesn't conform to the standard CarboKitten data
-layout, you may choose to not implement this function and implement
-`state_writer` and `frame_writer` instead. The same goes for `write_production`,
-`write_disintegration` and `write_deposition`.
+If your output object type doesn't conform to the standard CarboKitten data layout, you may
+choose to not implement this function and implement `state_writer` and `frame_writer` instead.
+The same goes for `write_production`, `write_disintegration` and `write_deposition`.
 """
-function write_bathymetry end
+function write_sediment_thickness end
 
 """
     write_active_layer(out::T, name::Symbol, idx::Int, data::AbstractArray{Amount, dim}) where {T, dim}
@@ -265,9 +257,9 @@ function state_writer(input::Input, out) where {Input <: AbstractInput}
     return function (idx::Int, state::AbstractState)
         for (k, v) in output_sets
             if mod(idx - 1, v.write_interval) == 0
-                write_bathymetry(
+                write_sediment_thickness(
                     out, k, div(idx - 1, v.write_interval) + 1,
-                    view(state.bathymetry, v.slice...))
+                    view(state.sediment_height, v.slice...))
 
                 if save_active_layer
                     write_active_layer(

4c8477b154ed65e5030745cd7de02981772467f7
csummers25 <167093840+csummers25@users.noreply.github.com>
2026-09-21 15:46:22 +0200

    add dominant_facies tests

--- test/DenudationSpec.jl
+++ test/DenudationSpec.jl
@@ -1,24 +1,33 @@
+module DenudationSpec
 using Test
 using Unitful
 
+using CarboKitten
+using CarboKitten.Denudation.DissolutionMod: dissolution, dominant_facies
+using CarboKitten.SedimentStack: push_sediment!, peek_sediment
+using CarboKitten.Components.Common
+using CarboKitten.Components: Denudation as D
+
+
+#import CarboKitten.Denudation.PhysicalErosionMod: physical_erosion, mass_erosion, total_mass_redistribution
+using CarboKitten.Stencil: Periodic, Reflected, stencil
+using CarboKitten.Config: Vectors, TimeProperties
+using CarboKitten.Boxes: Box
+using CarboKitten.Models: WithDenudation as WD
+using CarboKitten.Denudation.EmpiricalDenudationMod: slope_kernel
+using CarboKitten.Denudation: denudation, redistribution, Dissolution, NoDenudation, PhysicalErosion, EmpiricalDenudation
+
+
 @testset "DenudationTST" begin
-    import CarboKitten.Denudation.DissolutionMod: dissolution
-    import CarboKitten.Denudation.EmpiricalDenudationMod: empirical_denudation, slope_kernel
-    import CarboKitten.Denudation.PhysicalErosionMod: physical_erosion, mass_erosion, total_mass_redistribution
-    using CarboKitten.Stencil: Periodic, Reflected, stencil
-    using CarboKitten.Config: Vectors, TimeProperties
-    using CarboKitten.Boxes: Box
-    using CarboKitten.Models.WithDenudation: Input, Facies
-    using CarboKitten.Denudation: denudation, redistribution, Dissolution, NoDenudation, PhysicalErosion, EmpiricalDenudation
-
-
+ 
     DENUDATION_HIGH_CO2 = Dissolution(temp = 293.0u"K",precip = 1.0u"m/yr", pco2 = 10^(-1.5)*u"atm",reactionrate = 2e-3u"m/yr")
     DENUDATION_LOW_CO2 = Dissolution(temp = 293.0u"K",precip = 1.0u"m/yr", pco2 = 10^(-2.5)*u"atm",reactionrate = 2e-3u"m/yr")
     DENUDATION_LOW_P = EmpiricalDenudation(precip = 0.8u"m/yr")
     DENUDATION_HIGH_P = EmpiricalDenudation(precip = 1.0u"m/yr")
     DENUDATION_PHYS = PhysicalErosion()
+    
     MODEL1 = [
-        Facies(viability_range = (4, 10),
+        WD.Facies(viability_range = (4, 10),
         activation_range = (6, 10),
         production = BenthicProduction(
             maximum_growth_rate = 500u"m/Myr",
@@ -30,7 +39,7 @@ using Unitful
         infiltration_coefficient= 0.5,
         erodibility = 0.23u"m/yr"),
 
-        Facies(viability_range = (4, 10),
+        WD.Facies(viability_range = (4, 10),
         activation_range = (6, 10),
         production = BenthicProduction(
             maximum_growth_rate = 400u"m/Myr",
@@ -42,7 +51,7 @@ using Unitful
         infiltration_coefficient= 0.5,
         erodibility = 0.23u"m/yr"),
 
-        Facies(viability_range = (4, 10),
+        WD.Facies(viability_range = (4, 10),
         activation_range = (6, 10),
         production = BenthicProduction(
             maximum_growth_rate = 100u"m/Myr",
@@ -63,11 +72,15 @@ using Unitful
                 1  3  3  3  0
                 1  3  2  3  2]
 
-    struct test_state
-        ca::Array{Int}
-    end
 
-    STATE1 = test_state(ca_init)
+    STATE1 = WD.State(
+        step=0,
+        bathymetry=zeros(Height, box.grid_size...),
+        sediment_thickness=zeros(Height, box.grid_size...),
+        sediment_buffer=zeros(Float64, 10, n_facies, box.grid_size...),
+        active_layer=zeros(Amount, n_facies, box.grid_size...),
+        ca=ca_init, ca_priority=[1,2,3]
+    )
 
     denudation_mass_HIGH_CO2 = zeros(typeof(0.0u"m/kyr"),n_facies,box.grid_size...)
     denudation_mass_LOW_CO2 = zeros(typeof(0.0u"m/kyr"),n_facies,box.grid_size...)
@@ -77,11 +90,12 @@ using Unitful
     denudation_mass_phys_flat = zeros(typeof(0.0u"m/kyr"),n_facies,box.grid_size...)
     redistribution_mass = zeros(typeof(0.0u"m"),n_facies,box.grid_size...)
 
-    water_depth = -100 .* [ 0.989943  0.48076   0.518983  0.997996   0.895681
-                    0.872733  0.208779  0.882917  0.550494   0.674066
-                    0.57987   0.619433  0.769506  0.593786   0.856186
-                    0.407728  0.469545  0.896348  0.473817   0.797112
+    water_depth = -100 .* [ 0.989943  0.48076   0.518983  0.997996   0.895681;
+                    0.872733  0.208779  0.882917  0.550494   0.674066;
+                    0.57987   0.619433  0.769506  0.593786   0.856186;
+                    0.407728  0.469545  0.896348  0.473817   0.797112;
                     0.610194  0.921632  0.322729  0.0103646  0.691191]
+
     water_depth_flat = -0.5 .* ones(box.grid_size...)
     inf_map = ones(box.grid_size...)
     slope = rand(Float64, box.grid_size...)
@@ -106,16 +120,45 @@ using Unitful
     inf_map[idx] = MODEL1[f].infiltration_coefficient
     end
 
-    (redistribution_mass) = redistribution(box,DENUDATION_PHYS,denudation_mass_phys .*1.0u"Myr",water_depth)
+    # needs restructuring to include the sediment buffer, sediment thickness
+    # the resdistribution function should be applied to sediment popped from the buffer
+    # not on the denuded mass since this is just the total mass, not split by facies
+    # also now that the sediment_thickness is checked, none of these do any denudation!
 
     @test sum(denudation_mass_HIGH_CO2) > sum(denudation_mass_LOW_CO2)
     @test sum(denudation_mass_LOW_P) < sum(denudation_mass_HIGH_P)
     @test sum(denudation_mass_phys) > sum(denudation_mass_phys_flat)
+
+    (redistribution_mass) = redistribution(box,DENUDATION_PHYS,denudation_mass_phys .*1.0u"Myr",water_depth)
     @test sum(denudation_mass_phys .*1.0u"Myr") ≈ sum(redistribution_mass)
 
-    #regression_test
-    #@test 1.8*0.95 < abs.(sum(denudation_mass_HIGH_CO2)) ./u"m/kyr" < 1.8 *1.05
-    #@test 477*0.95 < abs.(sum(denudation_mass_LOW_P)) ./u"m/kyr" < 477*1.05
-    #@test 0.15* 0.7115 < sum(denudation_mass_phys) ./u"m/kyr" < 0.15*1.05
+end
+
+@testset "dominant_facies" begin
+    state = D.State(
+        step=0,
+        bathymetry=zeros(Height, 3,3),
+        sediment_thickness=zeros(Height, 3,3),
+        sediment_buffer = zeros(Float64,10,3,3,3)
+    )
+    some_sed = zeros(3,3,3)
+    some_sed[1,:,:] .+= 1.0 # 1 is extactly enough to tip over into seocnd bucket, so first is empty!
+    push_sediment!(state.sediment_buffer, some_sed)
+    @test dominant_facies(state, CartesianIndex(1,1)) == 1    
+
+    some_sed[2,:,:] .+= 2.0 
+    push_sediment!(state.sediment_buffer, some_sed)
+    @test dominant_facies(state, CartesianIndex(1,1)) == 2
+
+    some_sed[3,:,:] .+= 3.0 
+    push_sediment!(state.sediment_buffer, some_sed)
+    @test dominant_facies(state, CartesianIndex(1,1)) == 3
+
+    # if there's no maximum, takes the first index
+    same_sed = ones(3,3,3)
+    push_sediment!(state.sediment_buffer, same_sed)
+    @test dominant_facies(state, CartesianIndex(1,1)) == 1
+
+end
 
 end

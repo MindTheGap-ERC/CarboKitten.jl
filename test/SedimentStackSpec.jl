e0c9a5e2ecd3529f653c91dfbade688513a1a33c
csummers25 <167093840+csummers25@users.noreply.github.com>
2026-09-21 15:44:34 +0200

    fixed edge case bug in peek_sediment, when exactly emptying buffer with int amount of sediment in

--- test/SedimentStackSpec.jl
+++ test/SedimentStackSpec.jl
@@ -1,6 +1,6 @@
 # ~/~ begin <<docs/src/components/sediment_buffer.md#test/SedimentStackSpec.jl>>[init]
 @testset "SedimentStack" begin
-  using CarboKitten.SedimentStack: push_sediment!, pop_sediment!
+  using CarboKitten.SedimentStack: push_sediment!, pop_sediment!, peek_sediment
   stack = zeros(Float64, 10, 3)
   @test pop_sediment!(stack, 0.0) == [0.0, 0.0, 0.0]
   push_sediment!(stack, [5.0, 0, 0])
@@ -9,6 +9,10 @@
   @test pop_sediment!(stack, 2.0) == [0.25, 1.75, 0.0]
   @test pop_sediment!(stack, 1.5) == [1.25, 0.25, 0.0]
   @test pop_sediment!(stack, 0.0) == [0.0, 0.0, 0.0]
+
+  stack2 = zeros(Float64, 10, 3)
+  push_sediment!(stack2, [1.0, 0.0, 0.0])
+  @test peek_sediment(stack2, 1.0) == [1.0, 0.0, 0.0]
 end
 
 @testset "SedimentArray" begin

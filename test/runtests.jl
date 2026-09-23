63be198f2cab68dce5187506fde9415abbf6957a
csummers25 <167093840+csummers25@users.noreply.github.com>
2026-09-23 14:20:06 +0200

    fix filename typo

--- test/runtests.jl
+++ test/runtests.jl
@@ -58,5 +58,5 @@ TEST_PATH = mktempdir()
 
     include("Models/CAPSpec.jl")
     include("Models/BS92Spec.jl")
-    include("Models/withoutCADenudationSpec.jl")
+    include("Models/WithoutCADenudationSpec.jl")
 end
